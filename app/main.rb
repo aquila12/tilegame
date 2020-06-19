283fc9b95759f3bff1a224d974b7eca32f1435a7
Nick Moriarty <njmoriarty@gmail.com>
2020-06-20 00:44:18 +0100

    Support animated tiles

--- app/main.rb
+++ app/main.rb
@@ -5,27 +5,29 @@ TILE_W = 80
 TILE_H = 40
 
 MAP = [
-  '................................',
-  '.LLLL....LLLL....LLLL....LLLL...',
-  '.LLLL....LLLL....LLLL....LLLL...',
-  '.LLLL....LLLL....LLLL....LLLL...',
-  '.LLLL....LLLL....LLLL....LLLL...',
-  '................................',
-  '................................',
-  '.LLLL....LLLL....LLLL....LLLL...',
-  '.LLLL....LLLL....LLLL....LLLL...',
-  '.LLLL....LLLL....LLLL....LLLL...',
-  '.LLLL....LLLL....LLLL....LLLL...',
-  '................................',
-  '................................',
-  '................................',
-  '................................',
-  '................................'
+  '................',
+  '.gg......,,.....',
+  '.gg......,,.....',
+  '.gg......,,.....',
+  '.gg......,,.....',
+  '.gg..ll.........',
+  '.gg..ll.........',
+  '.gg..ll..,,..gg.',
+  '.gg..ll..,,..gg.',
+  '.gg..ll..,,..gg.',
+  '.gg..ll..,,..gg.',
+  '.gg..ll.........',
+  '.gg..ll.........',
+  '................',
+  '................',
+  '................'
 ]
 
 TILESET = {
   '.' => { path: :active_tileset, x: TILE_W, y: 0 },
-  'L' => { path: :active_tileset, x: 0, y: TILE_H }
+  'g' => { path: :active_tileset, x: 0, y: TILE_H },
+  'l' => { path: :active_tileset, x: TILE_W, y: TILE_H },
+  ',' => { path: :active_tileset, x: 2*TILE_W, y: TILE_H, animated: 1 }
 }
 
 SEGMENT_DATA = { tilemap: MAP, tileset: TILESET }
@@ -41,6 +43,11 @@ class Game
     update_solid_tile(0, 0, 32, 32, 32)
     update_solid_tile(1, 0, 32, 32, 128)
     update_solid_tile(0, 1, 32, 192, 32)
+    update_solid_tile(1, 1, 255, 128, 64)
+    update_solid_tile(2, 1, 32, 32, 96)
+    update_solid_tile(3, 1, 32, 32, 128)
+    update_solid_tile(4, 1, 32, 32, 160)
+    update_solid_tile(5, 1, 32, 32, 128)
     @cam = [0, 0]
   end
 
@@ -59,7 +66,7 @@ class Game
 
   EXTENT = [1280, 720]
   MARGIN = 80
-  PAN_RATE = 8
+  PAN_RATE = 10
   def inputs
     pos = @args.inputs.mouse.position
     pan = 2.times.map { |dim|
@@ -73,12 +80,13 @@ class Game
   end
 
   def render
+    TILESET[','][:x] = (2 + (@args.tick_count/30).floor % 4)*TILE_W
     @tilemap.render(@args)
   end
 end
 
 def tick(args)
-  @game = Game.new(args) if args.tick_count == 0
+  $game = @game = Game.new(args) if args.tick_count == 0
   @game.tick
   FpsProfiler.tick
   args.outputs.labels << { x: 8, y: 720 - 8, text: FpsProfiler.report }
--- lib/tile_map.rb
+++ lib/tile_map.rb
@@ -1,56 +1,121 @@
-class TileMap
-  class Segment
-    SENTRY = { dirty: false, id: nil }
+def derp(sprite)
+  {
+    x: sprite.x,
+    y: sprite.y,
+    w: sprite.w,
+    h: sprite.h,
+    path: sprite.path,
+    source_x: sprite.source_x,
+    source_y: sprite.source_y,
+    source_w: sprite.source_w,
+    source_h: sprite.source_h
+  }
+end
 
-    def initialize(tiles_x, tiles_y, tile_w, tile_h, tileset)
-      @w = tiles_x
-      @h = tiles_y
-      @tile_w = tile_w
-      @tile_h = tile_h
+def vars(o)
+  h = {}
+  o.instance_variables.each do |v|
+    h[v] = o.instance_variable_get(v)
+  end
+  h
+end
+
+class TileMapSprite
+  attr_sprite
+
+  def initialize
+    @r = 255
+    @g = 255
+    @b = 255
+    @a = 255
+  end
+
+  def position(x, y)
+    @x = x
+    @y = y
+  end
+
+  def dimensions(w, h)
+    @source_w = @w = w
+    @source_h = @h = h
+  end
+
+  def source(path, x, y)
+    @path = path
+    @source_x = x
+    @source_y = y
+  end
+end
+
+class TileMap
+  class Segment < TileMapSprite
+    class TileSprite < TileMapSprite
+      attr_accessor :dirty, :animated
+
+      def initialize
+        super
+        @dirty = true
+      end
+
+      def set(tiledef)
+        @dirty = true
+        @tiledef = tiledef
+        @animated = tiledef[:animated]
+        source(@tiledef[:path], @tiledef[:x], @tiledef[:y])
+      end
+
+      def animate
+        @source_x = @tiledef[:x]
+        @source_y = @tiledef[:y]
+      end
+    end
+
+    SENTRY = TileSprite.new
+
+    attr_reader :ox, :oy
+
+    def initialize(tiles_x, tiles_y, tile_width, tile_height, tileset)
+      @tiles_x = tiles_x
+      @tiles_y = tiles_y
+      @tile_width = tile_width
+      @tile_height = tile_height
       @tileset = tileset
-      @tiles = (@w * @h).times.map { { dirty: true, id: nil } }
+      init_tileset
+    end
+
+    def init_tileset
+      @tiles = (@tiles_x * @tiles_y).times.map { TileSprite.new }
+      each_tile do |tile, x, y|
+        tile.position(x * @tile_width, y * @tile_height)
+        tile.dimensions(@tile_width, @tile_height)
+      end
+    end
+
+    def origin(ox, oy)
+      @ox = ox
+      @oy = oy
     end
 
     def tile(x, y)
-      return SENTRY if x < 0 || x >= @w || y < 0 || y >= @h
-      @tiles[@w * y + x]
+      return SENTRY if x < 0 || x >= @tiles_x || y < 0 || y >= @tiles_h
+      @tiles[@tiles_x * y + x]
     end
 
     def each_tile
-      @h.times { |y| @w.times { |x| yield(tile(x,y),x,y) } }
+      @tiles_y.times { |y| @tiles_x.times { |x| yield(tile(x,y),x,y) } }
     end
 
-    def _renderable_tile(tile, x, y)
-      tilesource = @tileset[tile[:id]]
-      {
-        x: (x * @tile_w), y: (y * @tile_h),
-        w: @tile_w, h: @tile_h,
-        path: tilesource[:path],
-        source_x: tilesource[:x],
-        source_y: tilesource[:y],
-        source_w: @tile_w,
-        source_h: @tile_h
-      }
-    end
-
-    def _renderable_tiles
-      list = []
-      each_tile do |tile, x, y|
-        next unless tile[:dirty]
-        list << _renderable_tile(tile, x, y)
-      end
+    def dequeue_renderable!
+      list = @tiles.find_all(&:dirty)
+      list.each { |t| t.animated ? t.animate : t.dirty=false }
       list
     end
 
     def load(data)
       each_tile do |tile, x, y|
-        tile[:id] = data[y][x]
+        id = data[y][x]
+        tile.set(@tileset[id])
       end
-    end
-
-    def render(target)
-      renderable = _renderable_tiles
-      target.sprites << renderable unless renderable.empty?
     end
   end
 
@@ -61,14 +126,16 @@ class TileMap
   #  which need to be recycled
   # Update loaded segments in case they have any animated tiles
 
-  def initialize(name, tile_w, tile_h, seg_size, &loader)
+  def initialize(name, tile_width, tile_height, seg_size, &loader)
+    super
     @name = name
     @loader = loader
     @seg_size = seg_size
-    @tile_w = tile_w
-    @tile_h = tile_h
-    @width = @seg_size * @tile_w
-    @height = @seg_size * @tile_h
+    @tile_width = tile_width
+    @tile_height = tile_height
+    @width = @seg_size * @tile_width
+    @height = @seg_size * @tile_height
+    raise ArgumentError, "Map segment too big: #{@width}×#{@height}" if @width > 1280 || @height > 720
     @segments = []
     pan_abs(0, 0)
   end
@@ -83,23 +150,37 @@ class TileMap
   end
 
   def load_segment(sx, sy)
-    name = "#{name}_#{sx.to_i}_#{sy.to_i}"
+    name = "#{@name}_#{sx.to_i}_#{sy.to_i}"
     segment_data = @loader.call(sx, sy)
-    s = Segment.new(@seg_size, @seg_size, @tile_w, @tile_h, segment_data[:tileset])
+    s = Segment.new(@seg_size, @seg_size, @tile_width, @tile_height, segment_data[:tileset])
+
+    s.origin(sx * @width, sy * @height)
+    s.dimensions(@width, @height)
+    s.source(name, 0, 0)
+
     s.load(segment_data[:tilemap])
-    @segments << {
-      sx: sx, sy: sy, x: sx * @width, y: sy * @height, segment: s, name: name
+    @segments << s
+  end
+
+  def _copy_spec(path)
+    {
+      x: 0, y: 0, w: @width, h: @height,
+      path: path, source_x: 0, source_y: 0, source_w: @width, source_h: @height
     }
   end
 
   def render(args)
     @segments.each do |s|
-      s[:segment].render(args.render_target(s[:name]))
+      renderable = s.dequeue_renderable!
+      unless renderable.empty?
+        args.render_target(:seg_scratch).sprites << _copy_spec(s.path)
+        args.render_target(s.path).sprites << _copy_spec(:seg_scratch)
+        args.render_target(s.path).sprites << renderable
+      end
 
-      args.outputs.sprites << {
-        x: s[:x] - @pan[:x], y: s[:y] - @pan[:y], w: @width, h: @height,
-        path: s[:name], source_x: 0, source_y: 0, source_w: @width, source_h: @height
-      }
+      s.position(s.ox - @pan[:x], s.oy - @pan[:y])
     end
+
+    args.outputs.sprites << @segments
   end
 end
